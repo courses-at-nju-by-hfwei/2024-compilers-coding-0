5b487e2ba755cf887345bd7ac045098662e2499a
hengxin(Hengfeng Wei) <hfwei@nju.edu.cn>
2024-05-14 23:01:49 +0800

    codegen: hard mode

--- src/main/java/codegen/CodeGenVisitor.java
+++ src/main/java/codegen/CodeGenVisitor.java
@@ -1,13 +1,12 @@
 package codegen;
 
-import org.antlr.v4.runtime.Token;
-
 import java.io.FileWriter;
 import java.io.IOException;
-import java.sql.Array;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import org.antlr.v4.runtime.Token;
 
+// String: temp variables
 public class CodeGenVisitor extends ControlBaseVisitor<String> {
   private final Deque<String> breakLabels = new ArrayDeque<>();
   private final FileWriter fileWriter;
@@ -24,38 +23,178 @@ public class CodeGenVisitor extends ControlBaseVisitor<String> {
 
     try {
       fileWriter.close();
-    } catch (IOException e) {
-      throw new RuntimeException(e);
+    } catch (IOException ioe) {
+      System.err.println(ioe.getMessage());
     }
 
     return null;
   }
 
-  // stat -> break
+  // expr -> ID
   @Override
-  public String visitBreakStat(ControlParser.BreakStatContext ctx) {
-    emit("br " + breakLabels.peek());
+  public String visitIdExpr(ControlParser.IdExprContext ctx) {
+    return ctx.getText();
+  }
+
+  // stat -> ID = expr
+  @Override
+  public String visitAssignStat(ControlParser.AssignStatContext ctx) {
+    String expr = visit(ctx.expr());
+    emitCode(ctx.ID().getText() + " = " + expr);
+
     return null;
   }
 
-  // stat -> while (bool) { stat }
+  // bool -> true
   @Override
-  public String visitWhileStat(ControlParser.WhileStatContext ctx) {
-    String beginLabel = getNewLabel("begin");
-    emit(beginLabel);
+  public String visitTrueExpr(ControlParser.TrueExprContext ctx) {
+    return ctx.getText();
+  }
 
+  // bool -> false
+  @Override
+  public String visitFalseExpr(ControlParser.FalseExprContext ctx) {
+    return ctx.getText();
+  }
+
+  // bool -> ! bool
+  @Override
+  public String visitNotExpr(ControlParser.NotExprContext ctx) {
+    String bool = visit(ctx.bool());
+
+    String temp = getNewTemp();
+    emitCode(temp + " = " + "NOT " + bool);
+
+    return temp;
+  }
+
+  // bool -> lhs = bool && rhs = bool
+  @Override
+  public String visitAndExpr(ControlParser.AndExprContext ctx) {
+    // original version
+    //    String lhs = visit(ctx.lhs);
+    //    String rhs = visit(ctx.rhs);
+    //
+    //    String temp = getNewTemp();
+    //    emitCode(temp + " = " + "AND " + lhs + " " + rhs);
+    //
+    //    return temp;
+
+    // short-circuit version
+    String lhs = visit(ctx.lhs);
+
+    String trueLabel = getNewLabel("and.true");
+    String falseLabel = getNewLabel("and.false");
+    String endLabel = getNewLabel("and.end");
+    emitCode("br " + lhs + " " + trueLabel + " " + falseLabel);
+
+    String temp = getNewTemp();
+
+    emitLabel(trueLabel);
+    String rhs = visit(ctx.rhs);
+    emitCode(temp + " = " + rhs);
+    emitCode("br " + endLabel);
+
+    emitLabel(falseLabel);
+    emitCode(temp + " = false");
+
+    emitLabel(endLabel);
+
+    return temp;
+  }
+
+  // bool -> lhs = bool || rhs = bool
+  @Override
+  public String visitOrExpr(ControlParser.OrExprContext ctx) {
+    // original version
+    //    String lhs = visit(ctx.lhs);
+    //    String rhs = visit(ctx.rhs);
+    //
+    //    String temp = getNewTemp();
+    //    emitCode(temp + " = " + "OR " + lhs + " " + rhs);
+    //
+    //    return temp;
+
+    // short-circuit version
+    String lhs = visit(ctx.lhs);
+
+    String trueLabel = getNewLabel("or.true");
+    String falseLabel = getNewLabel("or.false");
+    emitCode("br " + lhs + " " + trueLabel + " " + falseLabel);
+
+    String temp = getNewTemp();
+
+    emitLabel(falseLabel);
+    String rhs = visit(ctx.rhs);
+    emitCode(temp + " = " + "OR " + lhs + " " + rhs);
+    String endLabel = getNewLabel("or.end");
+    emitLabel("br " + endLabel);
+
+    emitLabel(trueLabel);
+    emitCode(temp + " = true");
+
+    emitLabel(endLabel);
+
+    return temp;
+
+    // short-circuit version (wrong version for class)
+//    String lhs = visit(ctx.lhs);
+//
+//    String trueLabel = getNewLabel("or.true");
+//    String falseLabel = getNewLabel("or.false");
+//    emitCode("br " + lhs + " " + trueLabel + " " + falseLabel);
+//
+//    emitLabel(falseLabel);
+//    String rhs = visit(ctx.rhs);
+//    String temp = getNewTemp();
+//    emitCode(temp + " = " + "OR " + lhs + " " + rhs);
+//    emitLabel(trueLabel);
+//
+//    return temp;
+  }
+
+  // bool -> lhs = exp REL rhs = exp
+  @Override
+  public String visitRelExpr(ControlParser.RelExprContext ctx) {
+    String lhs = visit(ctx.lhs);
+    String rhs = visit(ctx.rhs);
+
+    Token op = ctx.op;
+    String cond =
+        switch (op.getType()) {
+          case ControlLexer.GT -> "sgt"; // signed greater than
+          case ControlLexer.GE -> "sge"; // signed greater than or equal
+          case ControlLexer.EE -> "eq"; // equal
+          default -> throw new IllegalArgumentException("No such cond: " + op.getText());
+        };
+
+    String temp = getNewTemp();
+    emitCode(temp + " = " + "icmp " + cond + " " + lhs + " " + rhs);
+
+    return temp;
+  }
+
+  // stat -> first = stat second = stat
+  @Override
+  public String visitSeqStat(ControlParser.SeqStatContext ctx) {
+    visit(ctx.first);
+    visit(ctx.second);
+
+    return null;
+  }
+
+  // stat -> if (bool) { stat }
+  @Override
+  public String visitIfStat(ControlParser.IfStatContext ctx) {
     String bool = visit(ctx.bool());
 
     String trueLabel = getNewLabel("b.true");
     String falseLabel = getNewLabel("b.false");
-    emit("br " + bool + " " + trueLabel + " " + falseLabel);
+
+    emitCode("br " + bool + " " + trueLabel + " " + falseLabel);
 
     emitLabel(trueLabel);
-    breakLabels.push(falseLabel);
     visit(ctx.stat());
-    breakLabels.pop();
-    emit("br " + beginLabel);
-
     emitLabel(falseLabel);
 
     return null;
@@ -69,156 +208,49 @@ public class CodeGenVisitor extends ControlBaseVisitor<String> {
     String trueLabel = getNewLabel("b.true");
     String falseLabel = getNewLabel("b.false");
     String endLabel = getNewLabel("b.end");
-    emit("br " + bool + " " + trueLabel + " " + falseLabel);
+
+    emitCode("br " + bool + " " + trueLabel + " " + falseLabel);
 
     emitLabel(trueLabel);
     visit(ctx.ifStat);
-    emit("br " + endLabel);
+    emitCode("br " + endLabel);
 
     emitLabel(falseLabel);
     visit(ctx.elseStat);
-
     emitLabel(endLabel);
 
     return null;
   }
 
-  // stat -> if (bool) { stat }
+  // stat -> while (bool) { stat }
   @Override
-  public String visitIfStat(ControlParser.IfStatContext ctx) {
+  public String visitWhileStat(ControlParser.WhileStatContext ctx) {
+    String beginLabel = getNewLabel("begin");
+    emitLabel(beginLabel);
+
     String bool = visit(ctx.bool());
 
     String trueLabel = getNewLabel("b.true");
     String falseLabel = getNewLabel("b.false");
-    emit("br " + bool + " " + trueLabel + " " + falseLabel);
+    emitCode("br " + bool + " " + trueLabel + " " + falseLabel);
 
     emitLabel(trueLabel);
+    breakLabels.push(falseLabel);
     visit(ctx.stat());
+    breakLabels.pop();
+    emitCode("br " + beginLabel);
 
     emitLabel(falseLabel);
 
     return null;
   }
 
-  // stat -> first = stat second = stat
+  // stat -> break
   @Override
-  public String visitSeqStat(ControlParser.SeqStatContext ctx) {
-    visit(ctx.first);
-    visit(ctx.second);
+  public String visitBreakStat(ControlParser.BreakStatContext ctx) {
+    emitCode("br " + breakLabels.peek());
+
     return null;
-  }
-
-  // stat -> ID = expr
-  @Override
-  public String visitAssignStat(ControlParser.AssignStatContext ctx) {
-    String expr = visit(ctx.expr());
-    emit(ctx.ID().getText() + " = " + expr);
-    return null;
-  }
-
-  // bool -> lhs = exp REL rhs = exp
-  @Override
-  public String visitRelExpr(ControlParser.RelExprContext ctx) {
-    String lhs = visit(ctx.lhs);
-    String rhs = visit(ctx.rhs);
-
-    Token op = ctx.op;
-    String cond = switch (op.getType()) {
-      case ControlLexer.GT -> "sgt";  // sgt: signed greater than
-      case ControlLexer.GE -> "sge";  // sge: signed greater than or equal
-      case ControlLexer.EE -> "eq";
-      default -> throw new IllegalArgumentException("No such cond: " + op.getText());
-    };
-
-    String temp = getNewTemp();
-    emit(temp + " = " + "icmp " + cond + " " + lhs + " " + rhs);
-    return temp;
-  }
-
-  // bool -> lhs = bool && rhs = bool
-  @Override
-  public String visitAndExpr(ControlParser.AndExprContext ctx) {
-    String lhs = visit(ctx.lhs);
-
-    String trueLabel = getNewLabel("b1.true");
-    String falseLabel = getNewLabel("b1.false");
-    String endLabel = getNewLabel("b1.end");
-    emit("br " + lhs + " " + trueLabel + " " + falseLabel);
-
-    emitLabel(trueLabel);
-    String rhs = visit(ctx.rhs);
-    String temp = getNewTemp();
-    emit(temp + " = " + "AND " + lhs + " " + rhs);
-    emit("br " + endLabel);
-
-    emitLabel(falseLabel);
-    emit(temp + " = " + "false");
-
-    emitLabel(endLabel);
-
-    return temp;
-  }
-
-  // bool -> lhs = bool || rhs = bool
-  @Override
-  public String visitOrExpr(ControlParser.OrExprContext ctx) {
-    String lhs = visit(ctx.lhs);
-
-    String trueLabel = getNewLabel("or.true");
-    String falseLabel = getNewLabel("or.false");
-    String endLabel = getNewLabel("or.end");
-    emit("br " + lhs + " " + trueLabel + " " + falseLabel);
-
-    emitLabel(falseLabel);
-    String rhs = visit(ctx.rhs);
-    String temp = getNewTemp();
-    emit(temp + " = OR " + lhs + " " + rhs);
-    emit("br " + endLabel);
-
-    emitLabel(trueLabel);
-    emit(temp + " = true");
-
-    emitLabel(endLabel);
-
-    return temp;
-  }
-
-  // bool -> ! bool
-  @Override
-  public String visitNotExpr(ControlParser.NotExprContext ctx) {
-    String bool = visit(ctx.bool());
-    String temp = getNewTemp();
-    emit(temp + " = " + "NOT " + bool);
-    return temp;
-  }
-
-  @Override
-  public String visitFalseExpr(ControlParser.FalseExprContext ctx) {
-    String temp = getNewTemp();
-    emit(temp + " = " + ctx.getText());
-    return temp;
-  }
-
-  // B -> true
-  @Override
-  public String visitTrueExpr(ControlParser.TrueExprContext ctx) {
-    String temp = getNewTemp();
-    emit(temp + " = " + ctx.getText());
-    return temp;
-  }
-
-  @Override
-  public String visitIdExpr(ControlParser.IdExprContext ctx) {
-    String temp = getNewTemp();
-    emit(temp + " = " + ctx.getText());
-    return temp;
-  }
-
-  @Override
-  public String visitIntExpr(ControlParser.IntExprContext ctx) {
-    String temp = getNewTemp();
-    emit(temp + " = " + ctx.getText());
-    return temp;
   }
 
   private void emitLabel(String label) {
@@ -229,7 +261,7 @@ public class CodeGenVisitor extends ControlBaseVisitor<String> {
     }
   }
 
-  private void emit(String code) {
+  private void emitCode(String code) {
     try {
       fileWriter.write(code + '\n');
     } catch (IOException ioe) {
